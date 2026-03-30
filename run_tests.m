08a0227c67ca73c1c537207c44a16933a8c0fa27
Shubh-art278 <schopade278@gmail.com>
2026-03-30 09:57:14 +0530

    Update run_tests.m

--- run_tests.m
+++ run_tests.m
@@ -1,14 +1,14 @@
-import matlab.unittest.TestRunner
-import matlab.unittest.plugins.XMLPlugin
+%import matlab.unittest.TestRunner
+%import matlab.unittest.plugins.XMLPlugin
 
-suite = testsuite("tests");
+%suite = testsuite("tests");
 
-runner = TestRunner.withTextOutput;
+%runner = TestRunner.withTextOutput;
 
-runner.addPlugin(XMLPlugin.producingJUnitFormat("test-results.xml"));
+%runner.addPlugin(XMLPlugin.producingJUnitFormat("test-results.xml"));
 
-results = runner.run(suite);
+%results = runner.run(suite);
 
-assertSuccess(results)
-This generates:
-test-results.xml
+%assertSuccess(results)
+%This generates:
+%test-results.xml
